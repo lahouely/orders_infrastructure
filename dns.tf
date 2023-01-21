403b146aeec819c9a651ea6c473629358c431009
lahouely <lahouel.youssouf@gmail.com>
2023-01-21 13:50:09 +0100

    Added my public key

--- dns.tf
+++ dns.tf
@@ -1,7 +1,8 @@
 //namecheap api is unfortunatly too expensive.
 //We're using their sandbox.
 //In the real world we will have create and edit the DNS A records manually.
-
+//tested and validated, but currently disabled for deployment speed.
+/*
 resource "namecheap_domain_records" "primary_dns" {
   domain = "youcef.store"
   record {
@@ -17,3 +18,4 @@ resource "namecheap_domain_records" "primary_dns" {
     ttl      = 60
   }
 }
+*/
--- main.tf
+++ main.tf
@@ -1,11 +1,33 @@
-resource "azurerm_resource_group" "orders-rg" {
+resource "azurerm_resource_group" "NetworkWatcherRG" {
   location = var.location
-  name     = join("-", [var.location, var.environment, "orders-rg"])
+  name     = "NetworkWatcherRG"
+}
+
+resource "azurerm_resource_group" "orders-loadbalancer-public-ip-rg" {
+  location = var.location
+  name     = join("-", [var.location, var.environment, "orders-loadbalancer-public-ip-rg"])
 }
 
 resource "azurerm_public_ip" "orders-loadbalancer-public-ip" {
   name                = join("-", [var.location, var.environment, "orders-loadbalancer-public-ip"])
-  resource_group_name = azurerm_resource_group.orders-rg.name
-  location            = azurerm_resource_group.orders-rg.location
+  resource_group_name = azurerm_resource_group.orders-loadbalancer-public-ip-rg.name
+  location            = var.location
   allocation_method   = "Static"
+}
+
+resource "azurerm_resource_group" "orders-loadbalancer-vm-rg" {
+  location = var.location
+  name     = join("-", [var.location, var.environment, "orders-loadbalancer-vm-rg"])
+}
+
+resource "azurerm_resource_group" "youcef-key-rg" {
+  location = var.location
+  name     = join("-", [var.location, var.environment, "youcef-key-rg"])
+}
+
+resource "azurerm_ssh_public_key" "youcef-key" {
+  name                = "youcef-key"
+  resource_group_name = azurerm_resource_group.youcef-key-rg.name
+  location            = var.location
+  public_key          = file("~/.ssh/id_rsa.pub")
 }
