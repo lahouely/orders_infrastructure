aeb569199917bacdb72d66c93e63b6022d6764a4
lahouely <lahouel.youssouf@gmail.com>
2023-01-22 15:22:14 +0100

    Added FTP to the NSG

--- dns.tf
+++ dns.tf
@@ -2,20 +2,20 @@
 //We're using their sandbox.
 //In the real world we will have create and edit the DNS A records manually.
 //tested and validated, but currently disabled for deployment speed.
-/*
+
 resource "namecheap_domain_records" "primary_dns" {
   domain = "youcef.store"
   record {
     hostname = "dev"
     type     = "A"
-    address  = azurerm_public_ip.orders-loadbalancer-public-ip.ip_address
+    address  = azurerm_public_ip.orders-loadbalancer-vm-public-ip.ip_address
     ttl      = 60
   }
   record {
     hostname = "*"
     type     = "A"
-    address  = azurerm_public_ip.orders-loadbalancer-public-ip.ip_address
+    address  = azurerm_public_ip.orders-loadbalancer-vm-public-ip.ip_address
     ttl      = 60
   }
 }
-*/
+
