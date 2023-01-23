49f6be178164b5d8b0e230100efe7c88edcf5a5b
lahouely <lahouel.youssouf@gmail.com>
2023-01-23 23:52:37 +0100

    Added Kubernetes provider

--- provider.tf
+++ provider.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "namecheap/namecheap"
       version = ">= 2.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
   }
 }
 
@@ -21,6 +25,14 @@ provider "azurerm" {
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
 }
+
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.orders-k8s.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.orders-k8s.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.orders-k8s.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.orders-k8s.kube_config.0.cluster_ca_certificate)
+}
+
 
 //NameCheap api is unfortunatly too expensive.
 //We're using their sandbox..
