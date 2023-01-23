a495ee57a3395f97d9053b603990b11c9ee5066c
lahouely <lahouel.youssouf@gmail.com>
2023-01-24 00:01:50 +0100

    Added Kubernetes deployment and lb service

--- provider.tf
+++ provider.tf
@@ -27,12 +27,11 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.orders-k8s.kube_config.0.host
+  host                   = azurerm_kubernetes_cluster.orders-k8s.kube_config.0.host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.orders-k8s.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.orders-k8s.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.orders-k8s.kube_config.0.cluster_ca_certificate)
 }
-
 
 //NameCheap api is unfortunatly too expensive.
 //We're using their sandbox..
