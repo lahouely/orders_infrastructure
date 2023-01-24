9200dda7a00a0be46604cc60ac673184e9513ba9
lahouely <lahouel.youssouf@gmail.com>
2023-01-24 01:11:28 +0100

    ()

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -38,11 +38,11 @@ resource "azurerm_kubernetes_cluster" "orders-k8s" {
   location            = var.location
   resource_group_name = azurerm_resource_group.orders-aks-rg.name
   node_resource_group = "${var.location}-${var.environment}-orders-k8s-managed-cluster-rg"
-  vnet_subnet_id = azurerm_subnet.orders-aks-vnet-subnet01.id
   default_node_pool {
     name       = "nodepool"
     vm_size    = "Standard_B2s"
     node_count = var.node_count
+    vnet_subnet_id = azurerm_subnet.orders-aks-vnet-subnet01.id
   }
   linux_profile {
     admin_username = var.admin_user
