d4e795f0b7f516e16cd662031b16e986df729a16
lahouely <lahouel.youssouf@gmail.com>
2023-01-29 19:11:54 +0100

    Created the DB and disabled SSL requirement

--- db_server.tf
+++ db_server.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "orders-db-rg" {
   name     = join("-", [var.location, var.environment, "orders-db-rg"])
 }
 
-resource "azurerm_mysql_flexible_server" "orders-db-msql-flexible-server" {
-  name                   = "orders-db-msql-flexible-server"
+resource "azurerm_mysql_flexible_server" "orders-db-mysql-flexible-server" {
+  name                   = "orders-db-mysql-flexible-server"
   resource_group_name    = azurerm_resource_group.orders-db-rg.name
   location               = var.location
   administrator_login    = var.admin_user
@@ -13,5 +13,25 @@ resource "azurerm_mysql_flexible_server" "orders-db-msql-flexible-server" {
   delegated_subnet_id    = azurerm_subnet.orders-db-vnet-default-subnet.id
   private_dns_zone_id    = azurerm_private_dns_zone.orders-db-private-dns-zone.id
   sku_name               = "B_Standard_B1ms"
+  storage{
+    auto_grow_enabled = false
+    iops = 360
+    size_gb = 20
+  }
   depends_on             = [azurerm_private_dns_zone_virtual_network_link.orders-db-private-dns-zone-vnet-link]
+}
+
+resource "azurerm_mysql_flexible_server_configuration" "orders-db-mysql-flexible-server-no-ssl" {
+  name                = "require_secure_transport"
+  resource_group_name = azurerm_resource_group.orders-db-rg.name
+  server_name         = azurerm_mysql_flexible_server.orders-db-mysql-flexible-server.name
+  value               = "OFF"
+}
+
+resource "azurerm_mysql_flexible_database" "example" {
+  name                = "orders-db"
+  resource_group_name = azurerm_resource_group.orders-db-rg.name
+  server_name         = azurerm_mysql_flexible_server.orders-db-mysql-flexible-server.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
